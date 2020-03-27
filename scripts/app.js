b44f880df7920f67657b0f07c1f5c5f8af747fcc
jolenewai <jolene.wai@gmail.com>
2020-03-27 03:46:54 +0000

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+//foursquare clientid
+let clientid = "IRDBUTDRK0IU2A1O2LWFJPFUTNUSTQFZEOPP5QWVXOQ0LMDM"
+let clientsecret = "XX32BTHUQ3MU5QXPATTSHF4Z3ASR3VPE2W5F2FGZ21Z1B5ZW"
+
+$(function(){
+
+    
+})
