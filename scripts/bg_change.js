5425542f83797c09d09672d1e5d2814951498a55
jolenewai <jolene.wai@gmail.com>
2020-03-28 14:13:48 +0000

    frond-end added

---
+++ scripts/bg_change.js
@@ -0,0 +1,30 @@
+$(function(){
+
+    let bgImages = ["bg_image1.jpg", "bg_image2.jpg","bg_image3.jpg", "bg_image4.jpg", "bg_image5.jpg", "bg_image6.jpg","bg_image7.jpg", "bg_image8.jpg"]
+    let img = new Image()
+    let imgPath = '/images/'
+
+    function preload(){
+
+        for (let i of bgImages){
+            img.url = imgPath+bgImages[i]
+        }
+
+        changeImage()
+        setInterval(changeImage,8000)
+
+    }
+    function changeImage(){
+
+        let randNum = Math.floor(Math.random() * 8)
+
+        console.log(imgPath+bgImages[randNum])
+        $('body').css("background-image", "url(\'"+imgPath+bgImages[randNum]+"\')")
+        
+    }
+
+    preload()
+    
+    
+
+})
