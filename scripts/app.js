dd1c16cda1e1fadf8e537a1cfa4d7636b41fc855
jolenewai <jolene.wai@gmail.com>
2020-03-30 06:42:43 +0000

    add second layer

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,66 @@
 //foursquare clientid
-let clientId = "IRDBUTDRK0IU2A1O2LWFJPFUTNUSTQFZEOPP5QWVXOQ0LMDM"
-let clientSecret = "XX32BTHUQ3MU5QXPATTSHF4Z3ASR3VPE2W5F2FGZ21Z1B5ZW"
+// let clientId = "IRDBUTDRK0IU2A1O2LWFJPFUTNUSTQFZEOPP5QWVXOQ0LMDM"
+// let clientSecret = "XX32BTHUQ3MU5QXPATTSHF4Z3ASR3VPE2W5F2FGZ21Z1B5ZW"
 
 //foursquare api base URL
-let foursquareURL = "https://api.foursquare.com/v2/venues/search"
+// let foursquareURL = "https://api.foursquare.com/v2/venues/search"
 
-let natureCat = "4d4b7105d754a06377d81259"
-let foursquareParam = {
-        client_id: clientId, 
-        client_secret: clientSecret, 
-        near: "Singapore",
-        categoryId: natureCat,
-        v:"20200328"
-}
+// let natureCat = "4d4b7105d754a06377d81259"
+// let foursquareParam = {
+//         client_id: clientId, 
+//         client_secret: clientSecret, 
+//         near: "Singapore",
+//         categoryId: natureCat,
+//         v:"20200328"
+// }
 
-let foursquareData = null
+//Data from data.gov.sg
+
+//markers layer
+let parksAPI = "/data/parks-geojson.geojson"
+let parksLayer 
+
+//geojson polygon layer
+let nParksAPI = "/data/nparks-parks-geojson.geojson"
+let nParksLayer
+
 
 $(function(){
     
-    getData(foursquareURL,foursquareParam,startLoading)
+    getData(nParksAPI, startLoading)
 
-    function getData(url, param, startLoading){
+    function getData(url, startLoading){
+        let promise = [axios.get(parksAPI), axios.get(nParksAPI)]
+
         let data = null
-        axios.get(url, {params:param}).then(response => startLoading(response))
+        axios.all(promise).then(axios.spread(function(parks, nparks)){
+            
+        })
     }    
 
     function startLoading(loadedData){
-        foursquareData = loadedData.data
-        mapMarking()
-        console.log(loadedData.data)
-    }
-    
+        mapMarking(loadedData)
+    } 
 
-    function mapMarking(){
-        let natureGroup = L.layerGroup();
-        for (let f of foursquareData.response.venues){
-            let marker = L.marker([f.location.lat, f.location.lng],{icon: treeIcon}).bindPopup(f.name)
-                
-            natureGroup.addLayer(marker)
-        }
-        map.addLayer(natureGroup)
+    function mapMarking(r){
+        nParksLayer = L.geoJson(r.data,{
+            onEachFeature:(feature, layer) => {
+                layer.bindPopup(feature.properties.Description);
+            }
+        }).addTo(map);
+
+        nParksLayer.setStyle({
+            color:'green',
+            fill:'green'
+        })
+
+    
+        
+        
     }
+
+    
+    
     
     
     
