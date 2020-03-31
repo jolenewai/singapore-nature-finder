e24cc2938800123e631ab443263bd754ae03562a
jolenewai <jolene.wai@gmail.com>
2020-03-31 07:02:50 +0000

    ui change, added results.html

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
-//foursquare clientid
-<<<<<<< HEAD
+// //foursquare clientid
 // let clientId = "IRDBUTDRK0IU2A1O2LWFJPFUTNUSTQFZEOPP5QWVXOQ0LMDM"
 // let clientSecret = "XX32BTHUQ3MU5QXPATTSHF4Z3ASR3VPE2W5F2FGZ21Z1B5ZW"
 
-//foursquare api base URL
+// //foursquare api base URL
 // let foursquareURL = "https://api.foursquare.com/v2/venues/search"
 
 // let natureCat = "4d4b7105d754a06377d81259"
@@ -15,64 +14,88 @@
 //         v:"20200328"
 // }
 
-//Data from data.gov.sg
+// let foursquareData = null
 
-//markers layer
+
+//Parks and NParks data from Data.gov.sg
 let parksAPI = "/data/parks-geojson.geojson"
-let parksLayer 
-
-//geojson polygon layer
 let nParksAPI = "/data/nparks-parks-geojson.geojson"
-let nParksLayer
+let sgAPI = "/data/national-map-polygon-geojson.geojson"
+let parks
+let nParks
+let sgmap
+$(function () {
 
+    function getName() {
+        console.log()
+        $('div.leaflet-popup-content').onLoad(function () {
+            for (let p of ($('div.leaflet-popup-content'))) {
+                console.log(p)
+                if (p.hidden == false) {
 
-$(function(){
-    
-    getData(nParksAPI, startLoading)
+                    let allCol = p.find('td')
+                    console.log(allCol)
 
-    function getData(url, startLoading){
-        let promise = [axios.get(parksAPI), axios.get(nParksAPI)]
-
-        let data = null
-        axios.all(promise).then(axios.spread(function(parks, nparks)){
-            
-        })
-    }    
-
-    function startLoading(loadedData){
-        mapMarking(loadedData)
-    } 
-
-    function mapMarking(r){
-        nParksLayer = L.geoJson(r.data,{
-            onEachFeature:(feature, layer) => {
-                layer.bindPopup(feature.properties.Description);
+                    let parkname = allCol[6]
+                }
             }
-        }).addTo(map);
-
-        nParksLayer.setStyle({
-            color:'green',
-            fill:'green'
         })
-
-    
-        
-        
     }
 
-    
-    
-    
-    
-    
-    
 
-=======
-let clientid = "IRDBUTDRK0IU2A1O2LWFJPFUTNUSTQFZEOPP5QWVXOQ0LMDM"
-let clientsecret = "XX32BTHUQ3MU5QXPATTSHF4Z3ASR3VPE2W5F2FGZ21Z1B5ZW"
 
-$(function(){
+    let promises = [axios.get(parksAPI), axios.get(nParksAPI), axios.get(sgAPI)];
+axios.all(promises).then(axios.spread(function (parks, nparks, sgmap) {
 
-    
->>>>>>> b44f880df7920f67657b0f07c1f5c5f8af747fcc
+    // let sgmapLayer = L.geoJson(sgmap.data,{
+    //     onEachFeature:(feature, layer) => {
+    //         layer.bindPopup(feature.properties.Description);
+    //     }
+    // }).addTo(map);
+
+    // sgmapLayer.setStyle({
+    //     color:'#707070',
+    //     fill: '#707070',
+    //     stroke: false,
+    //     opacity: 1.0
+    // })
+
+    // create the markers for nature
+    let parksLayer = L.layerGroup();
+    for (let n of parks.data.features) {
+        let desc = n.properties.Description
+        // only show results with Park in the decription
+        if (desc.indexOf("Park") >= 0 && desc.indexOf("nparks") >= 0) {
+            let marker = L.marker([n.geometry.coordinates[1], n.geometry.coordinates[0]], { icon: treeIcon }).bindPopup(desc)
+            parksLayer.addLayer(marker);
+        }
+    }
+
+    parksLayer.addTo(map)
+
+    let nParksLayer = L.geoJson(nparks.data, {
+        onEachFeature: (feature, layer) => {
+            layer.bindPopup(feature.properties.Description);
+        }
+    }).addTo(map);
+
+    nParksLayer.setStyle({
+        color: '#99A139',
+        fillColor: '#476220',
+        weight: 1,
+        fillOpacity: 0.75
+    })
+
+    $('.leaflet-marker-icon').click(getName)
+
+
+
+
+
+
+}));
+
+
+
+
 })
--- scripts/map.js
+++ scripts/map.js
@@ -1,4 +1,5 @@
-let map = L.map("map-container").setView([1.3138,103.8159],12)
+let sgLl = [1.3521, 103.8198]
+let map = L.map("map-container").setView(sgLl,12)
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -9,15 +10,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
 
-<<<<<<< HEAD
 let treeIcon = L.icon({
     iconUrl: '../images/tree_icon.png',
-    iconSize: [32, 60],
-    iconAnchor: [22, 94],
+    iconSize: [16, 30],
+    iconAnchor: [8, 30],
     popupAnchor: [-3, -76],
     shadowUrl: '../images/tree_icon_shadow.png',
-    shadowSize: [65, 43],
-    shadowAnchor: [22, 72]
-});
-=======
->>>>>>> b44f880df7920f67657b0f07c1f5c5f8af747fcc
+    shadowSize: [33, 17],
+    shadowAnchor: [9, 14]
+})
